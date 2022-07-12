5c7206fa63e16ab6f96a284960e3adbc95448017
evgeniy.datskiy <evgenij.datskij@mail.ru>
2022-07-12 09:56:18 +0500

    add for/of

--- chapter5/operators.js
+++ chapter5/operators.js
@@ -36,3 +36,101 @@ while (count < 10) {
   console.log(count);
   count++;
 }
+// 5.4.2 do/while
+// для тех редких случаев когда хотябы один раз цикл должен быть выполнен. do должен заканчиваться ; (while можно просто в {} заключить)
+function printArray(a) {
+  let len = a.length,
+    i = 0;
+  if (len === 0) {
+    console.log("Пустой массив");
+  } else {
+    do {
+      console.log(a[i]);
+    } while (++i < len);
+  }
+}
+// 5.4.3 for
+// for (инициализация; проверка; инкрементирование) оператор
+//все что в () можно опускать, но ; обязательна!
+function tail(o) {
+  // возвращает последнее свойство
+  for (
+    ;
+    o.next;
+    o = o.next // пустое тело
+  )
+    return o;
+}
+// 5.4.4 for/of
+// для работы с итерируемыми обьектами. т.е. для работы с массивами, строками (и множествами) !
+let data = [1, 2, 3, 4, 5, 6, 7, 8, 9],
+  summ = 0;
+for (let elem of data) {
+  // elem присваивается элемент масссива data. делается тело цикла, затем присваивается следующий и так до конца массива
+  summ += elem; // если добавим data.push(sum) то создадим бесконечный цикл
+}
+console.log(summ);
+
+// for/of с обьектами
+// for of не может работать с обьектами
+let o = { x: 1, y: 2, z: 3 };
+// for (let element of o); // TypeError: o is not iterable
+// , но межет работать с их ключами:
+let keys = "";
+for (let k of Object.keys(o)) {
+  keys += k;
+}
+console.log(keys); //xyz
+
+// или со значениями:
+let sum = 0;
+for (let k of Object.values(o)) {
+  sum += k;
+}
+console.log(sum); // 6
+
+//  а если нужны и ключи и значения:
+let pairs = "";
+for (let [k, v] of Object.entries(o)) {
+  pairs += k + v;
+}
+console.log(pairs);
+// Object.entries(o) - метод возвращающий массив массивов где каждый массив - пара ключ/значение
+
+// for/of со строками
+// Строки итерируются посимвольно:
+let frequency = {};
+for (let letter of "missisippi") {
+  if (frequency[letter]) {
+    frequency[letter]++;
+  } else {
+    frequency[letter] = 1;
+  }
+}
+console.log(frequency); //{m: 1, i: 4, s: 3, p: 2}
+
+// for/of  с классами Map и Set
+let text = "Na na na na na Batman!";
+let wordSet = new Set(text.split(" ")); // Set разбивает массив по уникальным элементам, складывая их в обьект, где ключ это номер, а значение - уникальное значение
+let wordNumb = new Set([2, 3, 3, 2, 1, 12]);
+console.log(wordSet);
+console.log(wordNumb);
+
+// Map же работает с парой ключ/значение:
+let m = new Map([
+  [1, "one"],
+  [2, "two"],
+]);
+for (let [key, value] of m) {
+  // Каждый раз во время итерации итератор возвращает массив
+  key;
+  value;
+}
+console.log(m);
+// for/await - для работы с асинхронными запросами
+// Для чтения порции асинхронно итерируемого потока данных и вывода их:
+async function printStream(stream) {
+  for await (let chunk of stream) {
+    console.log(chunk);
+  }
+}
