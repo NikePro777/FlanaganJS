9993a1e643b4c246d827773aea0b7c850633d83f
Evgeniy Datskij <evgenij.datskij@mail.ru>
2022-10-15 16:28:11 +0500

    typedMap

--- chapter9/class4.js
+++ chapter9/class4.js
@@ -46,3 +46,66 @@ Span.prototype.constructor = Span;
 Span.prototype.toString = function () {
   return `(${this.from}... +${this.to - this.from})`;
 };
+
+// 9.5.2 Создание подклассов с использованием extends и super
+// Cоздадим подклас от Array и добавим методы получения первого и последнего элементов
+class EZArray extends Array {
+  get first() {
+    return this[0];
+  }
+  last() {
+    return this[this.length - 1];
+  }
+} // т.е. мы полностью наследуем все свойства array и при этом у нас есть два своих
+let a = new EZArray();
+console.log(a instanceof EZArray); // true
+console.log(a instanceof Array); // true
+a.push(1, 2, 3, 4);
+console.log(a.length); //4 т.е. мы используем все методы унаследованные от Array
+a.pop();
+console.log(a);
+console.log(a.first);
+console.log(a.last()); // ну и естественно работают наши
+console.log(Array.isArray(a)); // true действительно является массивом
+console.log(EZArray.isArray(a)); // true подкласс наследует и статические методы
+
+//9.6 Теперь сделаем более сложный пример унаследуем класс от Map:
+class TypedMap extends Map {
+  constructor(keyType, valueType, entries) {
+    // если entries есть, то определим их типы:
+    if (entries) {
+      for (let [k, v] of entries) {
+        if (typeof k !== keyType || typeof v !== valueType) {
+          throw new TypeError(`Неправильный тип для записи [${k}, ${v}]`);
+        }
+      }
+    }
+    // Инициализируем суперкласс начальными записями (с проверенными типами)
+    super(entries);
+    // инициализируем класс this сохранив типы
+    this.keyType = keyType;
+    this.valueType = valueType;
+  }
+
+  // Теперь переопределим метод set() добавив проверку типов для любых новых записей, добавляемых к отображению
+  set(key, value) {
+    // Генерируем ошибку если ключ или значение имеют неправильный тип
+    if (this.keyType && typeof key !== this.keyType) {
+      throw new TypeError(`${key} не относится к типу ${this.keyType}`);
+    }
+    if (this.valueType && typeof value !== this.valueType) {
+      throw new TypeError(`${value} не относится к типу ${this.valueType}`);
+    }
+
+    // Если все корректно, вызываем версию метода set из суперкласса для фактическкогто добавление записи в отображение
+    // мф возвращаем то, что возвращает метод суперкласса
+    return super.set(key, value); // тут очень важно, мы используем super, т.е. вызываем метод set не который мы переопределили, а который есть у нашего суперкласса Map
+  }
+}
+
+// Использование super() в конструкторах
+// если мы определяем класс с ключевым словом extends тогда super() обязательно должно быть для вызова конструктора суперкласса
+// если мы не определили конструктор, то он будет автоматически определен
+// мы не можем использовать this в конструкторе пока не вызовем конструткор суперкласса методом super()
+// new.target является ссылкой на вызванный конструктор. Говоря по русски когда применяется метод super конструктор суперкласса видит это как new.target
+// п о хорошему суперкласс не должен знать создаются ли из него подклассы, но иногда полезно применять new.target.name
